d07b5ad8076e1b851d8a6b63622027f9666bb5ac
syedsaudsaad <s.saud15065@gmail.com>
2025-04-11 00:14:54 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ for i in range(1,16):
     students = {
         "ID": i,
         "Name":random.choice(names),
-        "Age":random.random(18, 25),
+        "Age":random.randin(18, 25),
          "Grade":random.choice(["A","B","C","D","E","F"]),
          "Marks":random.randint(40,100)
     }
